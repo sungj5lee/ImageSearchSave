ab3ac0554173f7579cb7c74eb702085cc96f0945
이성재 <sj5lee1@gmail.com>
2024-01-30 16:56:15 +0900

    made item layout added viewbounding to gradle

--- app/src/main/java/com/example/imagesearchsave/ImageSave.kt
+++ app/src/main/java/com/example/imagesearchsave/ImageSave.kt
@@ -1,4 +1,12 @@
 package com.example.imagesearchsave
 
-class ImageSave {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageSave : AppCompatActivity() {
+    private val binding by lazy { FragmentSaveBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
--- app/src/main/java/com/example/imagesearchsave/ImageSearch.kt
+++ app/src/main/java/com/example/imagesearchsave/ImageSearch.kt
@@ -1,4 +1,12 @@
 package com.example.imagesearchsave
 
-class ImageSearch {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageSearch : AppCompatActivity() {
+    private val binding by lazy { FragmentSearchBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
